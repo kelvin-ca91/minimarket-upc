fb0f34be8ca4f9af7ad21e73785cf4af1945e20f
Kelvin Carrion Alfaro <kelvin.ca91@gmail.com>
2024-11-03 09:57:36 -0500

    add detail

--- app/src/main/java/com/example/minimarket/DetailProductActivity.kt
+++ app/src/main/java/com/example/minimarket/DetailProductActivity.kt
@@ -1,6 +1,7 @@
 package com.example.minimarket
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,7 @@ class DetailProductActivity : AppCompatActivity() {
             insets
         }
     }
+
+    
+
 }
