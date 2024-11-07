24ac6087319a01fd579188fbd81381f0ada350dd
Odar Medina <odmaein85@hotmail.com>
2024-11-07 00:05:38 -0500

    cambios pantallas Odar

---
+++ app/src/main/java/com/example/minimarket/model/Compra.kt
@@ -0,0 +1,9 @@
+package com.example.minimarket.model
+
+data class Compra(
+    val codigo: String,       // Código único de la compra (por ejemplo, "#0000123")
+    val tipoEntrega: String,  // Tipo de entrega (por ejemplo, "Delivery" o "Recogo en tienda")
+    val fecha: String,        // Fecha de la compra (por ejemplo, "10/10/2024")
+    val total: String,        // Total de la compra (por ejemplo, "S/ 15.00")
+    val estado: String        // Estado de la compra (por ejemplo, "Pendiente de atención" o "Entregado")
+)
---
+++ app/src/main/java/com/example/minimarket/view/MisComprasActivity.kt
@@ -0,0 +1,79 @@
+package com.example.minimarket.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.minimarket.R
+import com.example.minimarket.adapters.CompraAdapter
+import com.example.minimarket.model.Compra
+import com.example.minimarket.view.custom.CustomToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MisComprasActivity : AppCompatActivity() {
+    private lateinit var customToolbar: CustomToolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Ocultar el ActionBar en esta actividad
+        supportActionBar?.hide()
+
+        setContentView(R.layout.activity_mis_compras)
+
+        // Text toolbar
+        customToolbar = findViewById(R.id.custom_toolbar)
+        customToolbar.setTitle("Mis Compras")
+
+        // 1. Obtener una referencia al RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewCompras)
+
+        // 2. Configurar el LayoutManager para el RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // 3. Crear una lista de compras de ejemplo
+        val compras = listOf(
+            Compra("0000123", "Delivery", "10/10/2024", "S/ 15.00", "Pendiente de atención"),
+            Compra("0000110", "Recogo en tienda", "08/10/2024", "S/ 82.30", "Entregado"),
+            Compra("0000092", "Delivery", "04/10/2024", "S/ 125.20", "Entregado")
+        )
+
+        // 4. Crear una instancia de CompraAdapter con la lista de compras
+        val adapter = CompraAdapter(compras)
+
+        // 5. Asignar el adaptador al RecyclerView
+        recyclerView.adapter = adapter
+
+
+        // Configurar la barra de navegación inferior
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.inflateMenu(R.menu.bottom_nav_menu)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_categorias -> {
+                    // Acción para la opción Categorías
+                    Toast.makeText(this, "Categorías seleccionadas", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.menu_cesta -> {
+                    // Acción para la opción Mi cesta
+                    val intent = Intent(this, CestaActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.menu_compras -> {
+                    // Acción para la opción Mis compras
+                    Toast.makeText(this, "Compras seleccionadas", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+
+    }
+}
