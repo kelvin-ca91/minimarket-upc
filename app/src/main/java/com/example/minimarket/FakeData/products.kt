654ffb5cb16f34e1212150e64be16b64537ead77
Kelvin Carrion Alfaro <kelvin.ca91@gmail.com>
2024-11-07 21:40:13 -0500

    changes

--- app/src/main/java/com/example/minimarket/FakeData/products.kt
+++ app/src/main/java/com/example/minimarket/FakeData/products.kt
@@ -6,7 +6,7 @@ object ProductsRepository {
         IProduct(1, 1, "Coca Cola 600ml", R.drawable.coca_cola, 3.00),
         IProduct(2, 1, "Inca Kola 600ml", R.drawable.incakola_600ml, 3.00),
         IProduct(3, 1, "Inca Kola 2.5L", R.drawable.incakola225l, 8.00),
-        IProduct(3, 1, "Yogurt 1L", R.drawable.yogurt_laive_lucuma_1l, 6.00),
+        IProduct(4, 1, "Yogurt 1L", R.drawable.yogurt_laive_lucuma_1l, 6.00),
         IProduct(5, 2, "Choco chips", R.drawable.cookies, 2.00),
         IProduct(6, 3, "Jamonada", R.drawable.embutidos, 4.00)
     )
